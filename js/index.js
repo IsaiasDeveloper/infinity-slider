7528e514b04208c6fad95e7e7b3fb9873e61381b
Isaias Santana dos Santos <isaias@drmeducacao.com.br>
2022-10-15 23:11:02 -0300

    finalização do slide para dispositivo desktop

--- js/index.js
+++ js/index.js
@@ -9,8 +9,9 @@ const navNextButton = document.querySelector('[data-slide="nav-next-button"]');
 const controlsWapper = document.querySelector(
   '[data-slide="controls-wrapper"]'
 );
-const slideItems = document.querySelectorAll('[data-slide="item"]');
+let slideItems = document.querySelectorAll('[data-slide="item"]');
 let controlButtons;
+let slideInterval;
 
 const state = {
   startingPoint: 0,
@@ -18,6 +19,8 @@ const state = {
   currentPoint: 0,
   movement: 0,
   currentSlideIndex: 0,
+  autoPlay: true,
+  timeInterval: 0,
 };
 
 //Mudar slides
@@ -35,18 +38,23 @@ function getCenterPosition({ index }) {
   return position;
 }
 
-function setVisibleSlide({ index }) {
+function setVisibleSlide({ index, animate }) {
+  if (index === 0 || index === slideItems.length - 1) {
+    index = state.currentSlideIndex;
+  }
   const position = getCenterPosition({ index });
   state.currentSlideIndex = index;
+  slideList.style.transition = animate === true ? 'transform 1s' : 'none';
+  activeControlButton({ index });
   translateSlide({ position: position });
 }
 
 function nextSlide() {
-  setVisibleSlide({ index: state.currentSlideIndex + 1 });
+  setVisibleSlide({ index: state.currentSlideIndex + 1, animate: true });
 }
 
 function previousSlide() {
-  setVisibleSlide({ index: state.currentSlideIndex - 1 });
+  setVisibleSlide({ index: state.currentSlideIndex - 1, animate: true });
 }
 
 function createControlButtons() {
@@ -59,12 +67,51 @@ function createControlButtons() {
     controlsWapper.append(controlButton);
   });
 }
+
+function activeControlButton({ index }) {
+  const slideItem = slideItems[index];
+  const dataIndex = Number(slideItem.dataset.index);
+  const controlButton = controlButtons[dataIndex];
+  controlButtons.forEach(function (controlButtonItem) {
+    controlButtonItem.classList.remove('active');
+  });
+  if (controlButton) controlButton.classList.add('active');
+}
+
+function createSlideClone() {
+  const firstSlide = slideItems[0].cloneNode(true);
+  firstSlide.classList.add('slide-cloned');
+  firstSlide.dataset.index = slideItems.length;
+
+  const secundSlide = slideItems[1].cloneNode(true);
+  secundSlide.classList.add('slide-cloned');
+  secundSlide.dataset.index = slideItems.length + 1;
+
+  const lastSlide = slideItems[slideItems.length - 1].cloneNode(true);
+  lastSlide.classList.add('slide-cloned');
+  lastSlide.dataset.index = -1;
+
+  const penultimateSlide = slideItems[slideItems.length - 2].cloneNode(true);
+  penultimateSlide.classList.add('slide-cloned');
+  penultimateSlide.dataset.index = -2;
+
+  //Criar no final da lista
+  slideList.append(firstSlide);
+  slideList.append(secundSlide);
+  //Criar no início da lista
+  slideList.prepend(lastSlide);
+  slideList.prepend(penultimateSlide);
+
+  slideItems = document.querySelectorAll('[data-slide="item"]');
+}
+
 //Apertar
 function onMouseDown(event, index) {
   const slideItem = event.currentTarget;
   state.startingPoint = event.clientX;
   state.currentPoint = state.startingPoint - state.savedPosition;
   state.currentSlideIndex = index;
+  slideList.style.transition = 'none';
   slideItem.addEventListener('mousemove', onMouseMove);
 }
 //Evento de mover mouse
@@ -76,24 +123,41 @@ function onMouseMove(event, index) {
 //Soltar
 function noMouseUp(event) {
   const slideItem = event.currentTarget;
-  const slideWidth = slideItem.clientWidth;
   if (state.movement < -150) {
     nextSlide();
   } else if (state.movement > 150) {
     previousSlide();
   } else {
-    setVisibleSlide({ index: state.currentSlideIndex });
+    setVisibleSlide({ index: state.currentSlideIndex, animate: true });
   }
 
   slideItem.removeEventListener('mousemove', onMouseMove);
 }
 
 function onControlButtonClick(index) {
-  setVisibleSlide({ index });
+  setVisibleSlide({ index: index + 2, animate: true });
+}
+
+function onSlideListTransitionEnd() {
+  if (state.currentSlideIndex === slideItems.length - 2) {
+    setVisibleSlide({ index: 2, animate: false });
+  }
+  if (state.currentSlideIndex === 1) {
+    setVisibleSlide({ index: slideItems.length - 3, animate: false });
+  }
+}
+
+function setAutoPlay() {
+  if (state.autoPlay) {
+    slideInterval = setInterval(function () {
+      setVisibleSlide({ index: state.currentSlideIndex + 1, animate: true });
+    }, state.timeInterval);
+  }
 }
 
 function setListeners() {
   controlButtons = document.querySelectorAll('[data-slide="control-button"]');
+  slideItems = document.querySelectorAll('[data-slide="item"]');
 
   //Adicionar evento nos indicatons
   controlButtons.forEach(function (controlButton, index) {
@@ -118,14 +182,29 @@ function setListeners() {
 
   navNextButton.addEventListener('click', nextSlide);
   navPreviousButton.addEventListener('click', previousSlide);
+  //Evento para voltar o slide de forma que o usuário não perceba
+  slideList.addEventListener('transitionend', onSlideListTransitionEnd);
+  slideWrapper.addEventListener('mouseenter', function () {
+    clearInterval(slideInterval);
+  });
+  slideWrapper.addEventListener('mouseleave', function () {
+    setAutoPlay();
+  });
 }
 
-function initSlider() {
+function initSlider({
+  startAtIndex = 0,
+  autoPlay = true,
+  timeInterval = 3000,
+}) {
+  state.autoPlay = autoPlay;
+  state.timeInterval = timeInterval;
   createControlButtons();
+  createSlideClone();
   setListeners();
-  setVisibleSlide({ index: 0 });
+  setVisibleSlide({ index: startAtIndex + 2, animate: true });
+  setAutoPlay();
 }
-initSlider();
 
 //Pegar clicks na página
 // document.addEventListener('click', (event) => {
@@ -133,5 +212,5 @@ initSlider();
 //   console.log(elementClicked);
 // });
 
-//Parei em 2h35m08s do vídeo
+//Parei em 3h49m59s do vídeo
 //https://www.youtube.com/watch?v=eUwqZrgASM0
